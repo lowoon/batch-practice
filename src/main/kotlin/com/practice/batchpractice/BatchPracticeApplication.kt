2e09a28cfcf33442b752d4dbbbba1fa946306aac
lowoon <sangbo0229@gmail.com>
2021-01-25 12:45:14 +0900

    init

---
+++ src/main/kotlin/com/practice/batchpractice/BatchPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.practice.batchpractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BatchPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<BatchPracticeApplication>(*args)
+}
